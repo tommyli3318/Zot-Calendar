288f4e886067959506283cdf69d2e012c022ceee
DeltaSierra4 <60omega@gmail.com>
2019-02-16 10:45:11 -0800

    Finalizing student

--- student.py
+++ student.py
@@ -12,11 +12,18 @@ class student:
 	def set_name(self, new_name: str):
 		self.name = new_name
 	
+	#Add an assignment 
+	def add_grade(self, course_name: str, assignment_name: str, category: str, points_gained: int, points_possible: int):
+		for given_course in courselist:
+			if given_course.get_name() == course_name:
+				given_course.add_assignment(assignment_name, category, (points_gained, points_possible))
+		print("Course not found.") #Temporary error message if course isn't found
+	
 	#Accessor for the grade. Compute the current grade of a course.
 	def get_grade(self, course_name: str) -> float:
 		for given_course in courselist:
 			if given_course.get_name() == course_name:
-				return given_course.compute_grade()
+				return given_course.get_overall_score()
 		print("Course not found.") #Temporary error message if course isn't found
 		return 0.0
 	
@@ -27,4 +34,11 @@ class student:
 		#for index in range(len(categories)):										#OBSOLETE
 		#	cat_dict = {category: weights[index] for category in categories}		#OBSOLETE
 		self.courselist.append(course(course_name, cat_dict))
+	
+	#Remove a course from the student's course list
+	def remove_course(self, course_name: str, categories: list, weights: list) -> None:
+		for given_course in courselist:
+			if given_course.get_name() == course_name:
+				self.remove(given_course)
+		print("Course not found.") #Temporary error message if course isn't found
 	
