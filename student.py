d112968138328529a5e8753d71021c7842ea72c9
DeltaSierra4 <60omega@gmail.com>
2019-02-16 11:14:06 -0800

    Test code in student

--- student.py
+++ student.py
@@ -14,10 +14,13 @@ class student:
 	
 	#Add an assignment 
 	def add_grade(self, course_name: str, assignment_name: str, category: str, points_gained: int, points_possible: int):
-		for given_course in courselist:
+		course_found = False
+		for given_course in self.courselist:
 			if given_course.get_name() == course_name:
 				given_course.add_assignment(assignment_name, category, (points_gained, points_possible))
-		print("Course not found.") #Temporary error message if course isn't found
+				course_found = True
+		if not course_found:
+			print("Course not found.") #Temporary error message if course isn't found
 	
 	#Accessor for the grade. Compute the current grade of a course.
 	def get_grade(self, course_name: str) -> float:
@@ -37,7 +40,7 @@ class student:
 	
 	#Remove a course from the student's course list
 	def remove_course(self, course_name: str, categories: list, weights: list) -> None:
-		for given_course in courselist:
+		for given_course in self.courselist:
 			if given_course.get_name() == course_name:
 				self.remove(given_course)
 		print("Course not found.") #Temporary error message if course isn't found
@@ -50,7 +53,32 @@ me.set_name("Nathan")
 print("The new name is " + me.get_name())
 
 categories = ["Exam", "Quiz", "Assignment", "Lab"]
-weights = [.4, .3, .1, .1]
+weights = [.4, .3, .2, .1]
 me.add_course("OChem", categories, weights)
+me.add_grade("OChem", "Midterm1", "Exam", 77, 100)
+me.add_grade("OChem", "Midterm2", "Exam", 44, 100)
+me.add_grade("OChem", "Essay", "Assignment", 88, 97)
+me.add_grade("OChem", "Pop quiz 1", "Quiz", 3, 5)
+me.add_grade("OChem", "Pop quiz 2", "Quiz", 4, 5)
+me.add_grade("OChem", "Pop quiz 3", "Quiz", 4, 6)
+me.add_grade("OChem", "Distillation lab", "Lab", 10, 10)
+
+categories2 = ["Midterm", "Final"]
+weights2 = [.5, .5]
+me.add_course("Algorithm", categories2, weights2)
+me.add_grade("Algorithm", "Amazing midterm", "Exam", 75, 100)
+me.add_grade("Algorithm", "Fucking kill me", "Exam", 55, 100)
 
 print(me.get_grade("OChem"))
+print(me.get_grade("Algorithm"))
+
+
+
+me2 = student("Tedrick")
+categories3 = ["Midterm", "Final", "Project"]
+weights3 = [.25, .5, .25]
+me.add_course("OS", categories3, weights3)
+me.add_grade("OS", "Amazing midterm", "Midterm", 98, 100)
+me.add_grade("OS", "Amazing final", "Final", 86, 100)
+me.add_grade("OS", "Kernel assignment", "Project", 107, 100)
+print(me2.get_grade("OS"))
