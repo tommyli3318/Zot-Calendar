03f8b60af4ab99637cbe28cd66c14419847ee294
Tommy Li <tommyli3318@gmail.com>
2019-02-16 18:18:14 -0800

    flask+react

---
+++ flask-backend/static/react/precache-manifest.de640d864191e1a38392bb6258175ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "450a472931a815e8e4c5",
+    "url": "/static/react/js/runtime~main.450a4729.js"
+  },
+  {
+    "revision": "3462789a4191fae4ed78",
+    "url": "/static/react/js/main.3462789a.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/react/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "3462789a4191fae4ed78",
+    "url": "/static/react/css/main.3cc66422.chunk.css"
+  },
+  {
+    "revision": "e6afb5671359710b32bb9fd5114c3e73",
+    "url": "/static/react/../../templates/index.html"
+  }
+];
